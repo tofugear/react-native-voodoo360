532370d3a2a5625d3561ccd501f797ff9a0f618e
Ca Phun Ung <caphun@tofugear.com>
2016-03-18 11:50:40 +0800

    using azure cdn and added non-native voodoo (commented out)

--- example/index.js
+++ example/index.js
@@ -9,43 +9,45 @@ import React, {
   Dimensions,
 } from 'react-native';
 
+import Voodoo360 from './Voodoo360';
+
 const IMGS = [
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img01.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img02.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img03.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img04.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img05.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img06.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img07.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img08.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img09.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img10.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img11.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img12.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img13.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img14.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img15.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img16.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img17.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img18.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img19.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img20.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img21.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img22.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img23.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img24.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img25.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img26.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img27.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img28.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img29.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img30.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img31.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img32.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img33.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img34.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img35.jpg",
-"https://d38cdkbh6wd7m4.cloudfront.net/360/CFWB5005-B_SLB/Lv2/img36.jpg"
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img01.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img02.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img03.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img04.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img05.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img06.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img07.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img08.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img09.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img10.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img11.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img12.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img13.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img14.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img15.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img16.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img17.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img18.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img19.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img20.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img21.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img22.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img23.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img24.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img25.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img26.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img27.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img28.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img29.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img30.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img31.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img32.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img33.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img34.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img35.jpg",
+"https://omnitech-demo-static360.azureedge.net/360/CFWB5005-B_SLB/Lv2/img36.jpg"
 ];
 const {height, width} = Dimensions.get('window');
 
@@ -55,9 +57,8 @@ class Main extends Component {
   render() {
     return (
       <View style={{width: width, height: height}}>
-        <Text>Hello World!</Text>
         <RCTVoodoo360 sources={IMGS} style={{width: width, height: 500}} />
-        <Text>Text below</Text>
+        {/*<Voodoo360 images={IMGS} />*/}
       </View>
     );
   }
