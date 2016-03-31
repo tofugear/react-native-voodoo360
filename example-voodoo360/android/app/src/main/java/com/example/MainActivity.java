f603eb0c4c370344de94519b9d2b9276c41d89a2
roku <roku@tofugear.com>
2016-03-31 15:55:55 +0800

    example-voodoo360: upgrade RN to 0.22

--- example-voodoo360/android/app/src/main/java/com/example/MainActivity.java
+++ example-voodoo360/android/app/src/main/java/com/example/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+import com.rnfs.RNFSPackage;
 import com.tofugear.voodoo360.Voodoo360Package;
 
 import java.util.Arrays;
@@ -29,14 +30,15 @@ public class MainActivity extends ReactActivity {
         return BuildConfig.DEBUG;
     }
 
-   /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new RNFSPackage(),
         new Voodoo360Package()
       );
     }
