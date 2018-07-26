be4cdc1e85d2a3f74d344d92cb97d5a1e9f59d4a
Ca Phun Ung <caphun@tofugear.com>
2018-07-26 13:16:51 +0800

    Migrate deprecated `React.PropTypes` to `prop-types` package.

--- ReactNativeVoodoo360.js
+++ ReactNativeVoodoo360.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   requireNativeComponent,
-  View
+  View,
+  ViewProps,
 } from 'react-native';
 
 export default class Voodoo360 extends React.Component {
   static propTypes = {
-    ...View.propTypes,
-    sources: React.PropTypes.arrayOf(React.PropTypes.string),
-    // source: React.PropTypes.string,
+    ...ViewPropTypes,
+    sources: PropTypes.arrayOf(PropTypes.string),
+    // source: PropTypes.string,
   };
 
   constructor(props) {
